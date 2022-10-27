929f210ba7ede0ad5377cf659565f823b4cd9ffc
DiceglieA <angelodiceglie10@gmail.com>
2022-10-27 15:30:07 +0200

    determinate le variabili

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// const arrGroceryList = []
+
+const arrRisoPatateCozz = [
+    'aglio',
+    'olio',
+    'cipolla',
+    'przzemolo',
+    'pomodori',
+    'sale',
+    'olio',
+    'patate',
+    'formaggio',
+    'pepe',
+    'riso',
+    'cozze',
+    'zucchine',
+    'uova',
+    
+]
+
+const eleGroceryList = document.querySelector('.lista-spesa');
+const inputGroceryItem = document.querySelector('#input-spesa');
+const eleGroceryForm = document.querySelector('form');
+
+
+while ()
+
+
+// eleGroceryForm.addEventListener('submit', function(event) {
+// 	event.preventDefault();
+
+// 	if (inputGroceryItem.value) {
+		
+// 		arrGroceryList.push(inputGroceryItem.value);
+// 		eleGroceryList.innerHTML += `<li>${inputGroceryItem.value}</li>`;
+// 		inputGroceryItem.value = '';
+// 	}
+// });
+
+// for (let i = 0; i < arrRisoPatateCozz.length; i++) {
+// 	eleGroceryList.innerHTML += `<li>${arrRisoPatateCozz[i]}</li>`;
+// }
